271cb92c24767c0f23a5c7df4a65d88f1a627910
Avleen13 <54954813+Avleen13@users.noreply.github.com>
2019-11-14 14:46:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,50 +1,67 @@
-const courseName = `Tools and Workflow`;
-const courseCode = `WDDM-115`;
-const courseInstructor = `Kadeem Best`;
-const courseStart = `Fall 2019`;
-const courseWeeks = 15; // 1 class per week (assumed)
-const courseBreaks = true; // Boolean
-const courseDuration = 160; // Minutes per class
+/******* GLOABL VARIABLES/DATA ********/
+// Define all of your variables here, including Object and Array references
+
+const allCourses = [
+  {
+    name: `Tools and Workflow`,
+    code: `WDDM-115`,
+    instructor: `Kadeem Best`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{
+    name: `Applied Web Development`,
+    code: `WDDM-113`,
+    instructor: `Rocco Panacci`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  }
+];
 
 
-const course = {
-  name: `Tools and Workflow`,
-  code: `WDDM-115`,
-  instructor: `Kadeem Best`,
-  start: { term: `Fall`, year: 2019 },
-  weeks: 15,
-  breaks: true,
-  duration: 160
-};
+/************* FUNCTIONS *************/
+// Now store all the functions that will manipulate the data
 
-document.getElementById(`name`).innerHTML = course.name;
-document.getElementById(`code`).innerHTML = course.code;
-document.getElementById(`instructor`).innerHTML = course.instructor;
-document.getElementById(`start`).innerHTML = `${course.start.term} ${course.start.year}`;
-document.getElementById(`weeks`).innerHTML = course.weeks;
-document.getElementById(`duration`).innerHTML = getDurationFromMinutes(course.duration);
+function getDurationFromMinutes(minutes) {
+  
+  const hrs = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  // const mins = minutes - hrs * 60; // Could also use this
 
-/*
-LAB:
-1. Fix the output of the start term by turning the Object's properties into a String
-      Ex: { term: `Fall`, year: 2019 }  >>  `Fall 2019`
-2. Write a function that will take a Number of minutes and will return a human-readable String
-      Ex: 160  >>  `2 hr, 40 min`
-      Hints:
-        - Division: /
-        - Remainder: %
-        - Rounding:
-            - Standard rules:     Math.round(4.5)
-            - Always round up:    Math.ceil(1.001)
-            - Always round down:  Math.floor(1.999)
-*/
-
-
-    function getDurationFromMinutes(minutes) {
-        var num = minutes;
-       var hours = (num / 60);
-       var rhours = Math.floor(hours);
-       var minutes = (hours - rhours) * 60;
-       var rminutes = Math.round(minutes);
-       return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+  return `${hrs} hr, ${mins} min`;
 }
+
+// Function:
+// Parameters: course:Object
+// Return: String of HTML (<article>)
+function getCourseAsHtmlString(course) {
+  return `
+    <article class="course">
+      <h3 id="name">${course.name}</h3>
+      <ul>
+        <li>Course Code: <strong>${course.code}</strong></li>
+        <li>Instructor: <strong>${course.instructor}</strong></li>
+        <li>Start: <strong>${course.start.term} ${course.start.year}</strong></li>
+        <li>
+          Weeks: <strong>${course.weeks}</strong>
+          <ul>
+            <li>Includes Break: <strong>${course.breaks}</strong></li>
+          </ul>
+        </li>
+        <li>Duration: <strong>${getDurationFromMinutes(course.duration)}</strong></li>
+      </ul>
+    </article>`;
+}
+
+/************* EXECUTABLE *************/
+// Execute functions that will access data
+
+
+// LAB:
+// map() the Array to a new array of Strings (formatted as HTML)
+// join() the NEW Array to a new String of HTML instructions (<article>)
+// Assign the String to the document
+// Test by adding a new Object to allCourses
